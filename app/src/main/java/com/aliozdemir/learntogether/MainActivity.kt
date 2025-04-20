8609012cc04eda816469aa2b20de6b46573888c4
Ali Ozdemir <100178366+aliozdemirao@users.noreply.github.com>
2025-04-20 03:13:29 +0300

    feat: Implement Jetpack Compose article screen
    This commit introduces a new screen that displays an article about Jetpack Compose.
    
    - Added string resources for the article's title, short description, and long description.
    - Implemented the `ComposeArticleApp` composable function to display the article.
    - Created an `ArticleCard` composable for the article content.
    - Added `Image` composable in `ArticleCard` for showing `bg_compose_background.png`.
    - Used `Text` composables with appropriate styling for the title, short description, and long description.
    - Added a preview for `ComposeArticleApp`.
    - Added image to `drawable-nodpi` directory.
    - Changed `enableEdgeToEdge` to comment.
    - Changed `Scaffold` to `Surface`.

--- app/src/main/java/com/aliozdemir/learntogether/MainActivity.kt
+++ app/src/main/java/com/aliozdemir/learntogether/MainActivity.kt
@@ -4,26 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextAlign.Companion.Justify
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.aliozdemir.learntogether.ui.theme.LearnTogetherTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
             LearnTogetherTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ComposeArticleApp()
                 }
             }
         }
@@ -31,17 +43,55 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun ComposeArticleApp() {
+    ArticleCard(
+        title = stringResource(R.string.title_jetpack_compose_tutorial),
+        shortDescription = stringResource(R.string.compose_short_desc),
+        longDescription = stringResource(R.string.compose_long_desc),
+        imagePainter = painterResource(R.drawable.bg_compose_background)
     )
 }
 
-@Preview(showBackground = true)
+@Composable
+private fun ArticleCard(
+    title: String,
+    shortDescription: String,
+    longDescription: String,
+    imagePainter: Painter,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier = modifier) {
+        Image(painter = imagePainter, contentDescription = null)
+        Row {
+            Text(
+                text = title,
+                modifier = Modifier.padding(16.dp),
+                fontSize = 24.sp
+            )
+        }
+        Row {
+            Text(
+                text = shortDescription,
+                modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+                textAlign = Justify
+            )
+        }
+        Row {
+            Text(
+                text = longDescription,
+                modifier = Modifier.padding(16.dp),
+                textAlign = Justify
+            )
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     LearnTogetherTheme {
-        Greeting("Android")
+        ComposeArticleApp()
     }
 }
